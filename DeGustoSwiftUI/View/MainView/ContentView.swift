0228b9045f0e990f3486a79bbbef9bd3a9e9c99a
SierikovaTetiana <mercury1663@gmail.com>
2022-08-07 14:43:16 +0300

    implement auto scroll slider images

--- DeGustoSwiftUI/View/MainView/ContentView.swift
+++ DeGustoSwiftUI/View/MainView/ContentView.swift
@@ -13,57 +13,59 @@ struct ContentView: View {
     @ObservedObject var sliderImagesViewModel = SliderImagesViewModel()
     
     var body: some View {
-        NavigationView {
-            VStack {
-                TabView {
-                    ScrollView {
-                    VStack {
-                        MenuScrollView(sliderImagesViewModel: sliderImagesViewModel)
-                        MenuTableView(mainViewModel: mainViewModel)
-                    }
-                    }
-                    .tabItem {
-                        Image("menuButton").resizable().aspectRatio(contentMode: .fit)
-                        Text("Головна")
-                    }
-                    MapView()
-                        .tabItem {
-                            Image(systemName: "map")
-                            Text("Ми на мапі")
+        GeometryReader { geometry in
+            NavigationView {
+                VStack {
+                    TabView {
+                        ScrollView {
+                            VStack {
+                                MenuScrollView(sliderImagesViewModel: sliderImagesViewModel, screenWidth: geometry.size.width)
+                                MenuTableView(mainViewModel: mainViewModel)
+                            }
                         }
+                        .tabItem {
+                            Image("menuButton").resizable().aspectRatio(contentMode: .fit)
+                            Text("Головна")
+                        }
+                        MapView()
+                            .tabItem {
+                                Image(systemName: "map")
+                                Text("Ми на мапі")
+                            }
+                    }
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Image("logo").resizable().aspectRatio(contentMode: .fit)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Image("logo").resizable().aspectRatio(contentMode: .fit)
+                    }
                 }
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(leading: Button(action: {
-                // do something
-            }) {
-                Image(systemName: "person")
-                    .imageScale(.large)
-                    .foregroundColor(Color("darkGreen"))
-            }, trailing: Button(action: {
-                // do something
-            }) {
-                Image(systemName: "cart")
-                    .foregroundColor(Color("darkGreen"))
-                    .imageScale(.large)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading: Button(action: {
+                    // do something
+                }) {
+                    Image(systemName: "person")
+                        .imageScale(.large)
+                        .foregroundColor(Color("darkGreen"))
+                }, trailing: Button(action: {
+                    // do something
+                }) {
+                    Image(systemName: "cart")
+                        .foregroundColor(Color("darkGreen"))
+                        .imageScale(.large)
+                })
+            }.onAppear(perform: {
+                let tabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithOpaqueBackground()
+                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                self.sliderImagesViewModel.fetchSliderImages()
+                self.mainViewModel.fetchMenu {
+                    self.mainViewModel.fetchUserFavorites {
+                        self.mainViewModel.fetchSectionMenuImage()
+                    }
+                }
+                UserAutorization.userAutorization.autorizeUser()
             })
-        }.onAppear(perform: {
-            let tabBarAppearance = UITabBarAppearance()
-            tabBarAppearance.configureWithOpaqueBackground()
-            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-            self.sliderImagesViewModel.fetchSliderImages()
-            self.mainViewModel.fetchMenu {
-                self.mainViewModel.fetchUserFavorites {
-                    self.mainViewModel.fetchSectionMenuImage()
-                }
-            }
-            UserAutorization.userAutorization.autorizeUser()
-        })
+        }
     }
 }
 
--- DeGustoSwiftUI/View/MainView/MenuScrollView.swift
+++ DeGustoSwiftUI/View/MainView/MenuScrollView.swift
@@ -10,27 +10,39 @@ import SwiftUI
 struct MenuScrollView: View {
     
     @ObservedObject var sliderImagesViewModel: SliderImagesViewModel
+    @State private var currentIndex: Int = 0
+    @State private var timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    let screenWidth: CGFloat
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 20) {
-                ForEach(sliderImagesViewModel.sliderImages, id: \.self) { image in
-                    ImageView(sliderImages: image)
+        ScrollViewReader { scrollProxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack() {
+                    ForEach(0..<sliderImagesViewModel.sliderImages.count, id: \.self) { index in
+                        ImageView(sliderImages: sliderImagesViewModel.sliderImages[index], imageWidth: screenWidth)
+                            .id(index)
+                    }
                 }
             }
-            .padding(.horizontal)
+            .padding(.top)
+            .animation(.linear, value: sliderImagesViewModel.sliderImages)
+            .onReceive(timer) { _ in
+                currentIndex = currentIndex < sliderImagesViewModel.sliderImages.count - 1 ? currentIndex + 1 : 0
+                withAnimation {
+                    scrollProxy.scrollTo(currentIndex)
+                }
+            }
         }
-        .padding(.top)
-        .animation(.linear, value: sliderImagesViewModel.sliderImages)
     }
 }
 
 struct ImageView: View {
     let sliderImages: SliderImage
+    let imageWidth: CGFloat
     var body: some View {
         Image(uiImage: sliderImages.image)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 400, height: 200)
+            .frame(width: imageWidth, height: 200)
     }
 }
